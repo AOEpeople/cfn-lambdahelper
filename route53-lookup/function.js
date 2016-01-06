beffc4a05ff540dca77b1e6b26b822ab71fd4f07
Lee Saferite <lee.saferite@aoe.com>
2016-01-06 12:54:41 -0500

    Adding new Route53 FQDN Lookup Helper

---
+++ route53-lookup/function.js
@@ -0,0 +1,26 @@
+var r = require('cfn-response');
+var AWS = require('aws-sdk');
+
+exports.handler = function (event, context) {
+    console.log('REQUEST RECEIVED:\n', JSON.stringify(event));
+    if (event.RequestType == 'Delete') {
+        r.send(event, context, r.SUCCESS);
+        return;
+    }
+    var rp = event.ResourceProperties;
+    var r53 = new AWS.Route53();
+    r53.getHostedZone({Id: rp.HostedZoneId}, function (err, data) {
+        var resp = {};
+        if (err) {
+            resp.Error = 'getHostedZone call failed';
+            console.log(resp.Error + ':\n', err);
+            r.send(event, context, r.FAILED, resp);
+        } else {
+            resp.Domain = data.HostedZone.Name;
+            resp.Hostname = rp.Hostname.trim();
+            resp.FQDN = resp.Hostname + '.' + resp.Domain;
+            console.log('FQDN: ' + resp.FQDN);
+            r.send(event, context, r.SUCCESS, resp);
+        }
+    });
+};
