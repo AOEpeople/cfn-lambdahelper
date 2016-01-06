2092d3176c9be1ff6f72eb7b082eb3c3b4d19ab3
Lee Saferite <lee.saferite@aoe.com>
2016-01-06 11:23:26 -0500

    Large refactor to clean up, reorganize, and fix the package
    Moved each lambda function ito a subdirectory
    Standardized file names for each function
    Added per-function template for users that don't want all the functions
    Reformatted all the JS and CF templates
    Switched to using the minified file inclusion

--- README.md
+++ README.md
@@ -11,4 +11,15 @@ Includes:
 - Route53Updater
 - StackDeleter
 
-See examples...
+## Usage
+### Load your AWS keys into env vars
+Fish with the dotenv function:
+dotenv .env
+
+### Prepare "Development Environment"
+cd scripts/lambda
+sudo npm install aws-sdk lambda-local
+wget https://gist.githubusercontent.com/fbrnc/a042b8b8949596991e36/raw/1da1aee52d62b2482ed386d9be05c39b7b9b6856/cfn-response.js -O node_modules/cfn-response.js
+
+### Test function
+node_modules/lambda-local/bin/lambda-local -l ElastiCacheTagger.js -e ElastiCacheTagger.event.json
---
+++ r53-updater/function.js
@@ -0,0 +1,90 @@
+var r = require('cfn-response');
+var AWS = require('aws-sdk');
+
+exports.handler = function (event, context) {
+    console.log('REQUEST RECEIVED:\n', JSON.stringify(event));
+    if (event.RequestType == 'Delete') {
+        r.send(event, context, r.SUCCESS);
+        return;
+    }
+    var rp = event.ResourceProperties;
+    var resp = {};
+    var r53 = new AWS.Route53();
+    r53.getHostedZone({Id: rp.HostedZoneId}, function (err, data) {
+        if (err) {
+            resp.Error = 'getHostedZone call failed';
+            console.log(resp.Error + ':\n', err);
+            r.send(event, context, r.FAILED, resp);
+        } else {
+            var domain = data.HostedZone.Name;
+            console.log('Domain: ' + domain);
+            resp.Domain = domain;
+            var names = rp.Name.split(",").map(function (v) {
+                return v.trim();
+            });
+            var batch = [];
+            for (var i = 0; i < names.length; i++) {
+                batch.push(
+                    {
+                        Action: 'UPSERT',
+                        ResourceRecordSet: {
+                            Name: names[i] + '.' + domain,
+                            Type: 'A',
+                            AliasTarget: {
+                                HostedZoneId: rp.AliasTargetHostedZoneId,
+                                DNSName: rp.AliasTargetDNSName,
+                                EvaluateTargetHealth: false
+                            }
+                        }
+                    }
+                );
+            }
+            r53.changeResourceRecordSets(
+                {
+                    ChangeBatch: {
+                        Changes: batch,
+                        Comment: rp.Comment
+                    },
+                    HostedZoneId: rp.HostedZoneId
+                },
+                function (err, data) {
+                    if (err) {
+                        resp.Error = 'changeResourceRecordSets call failed';
+                        console.log(resp.Error + ':\n', err);
+                        r.send(event, context, r.FAILED, resp);
+                    } else {
+                        var changeId = data.ChangeInfo.Id.split('\/')[2];
+                        console.log('ChangeId: ' + changeId);
+                        resp.ChangeId = changeId;
+                        var check = function () {
+                            console.log('Polling...');
+                            r53.getChange(
+                                {
+                                    Id: changeId
+                                },
+                                function (err, data) {
+                                    if (err) {
+                                        resp.Error = 'changeResourceRecordSets call failed';
+                                        console.log(resp.Error + ':\n', err);
+                                        r.send(event, context, r.FAILED, resp);
+                                    } else {
+                                        var status = data.ChangeInfo.Status;
+                                        console.log('Status: ' + status);
+                                        if (status == 'PENDING') {
+                                            setTimeout(check, 3000);
+                                        } else {
+                                            resp.status = status;
+                                            console.log('DONE!');
+                                            r.send(event, context, r.SUCCESS, resp);
+                                        }
+                                    }
+                                }
+                            );
+                        };
+                        setTimeout(check, 3000);
+                    }
+                }
+            );
+        }
+    });
+};
