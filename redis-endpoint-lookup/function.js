3267afc3d978ac542be1aad6f6e035732fec7aa4
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-04-06 20:38:23 +0000

    Added Redis Endpoint Lookup

---
+++ redis-endpoint-lookup/function.js
@@ -0,0 +1,42 @@
+var response = require('cfn-response');
+exports.handler = function (event, context) {
+
+    console.log('REQUEST RECEIVED:\\n', JSON.stringify(event));
+    if (event.RequestType == 'Delete') {
+        response.send(event, context, response.SUCCESS);
+        return;
+    }
+
+    var CacheClusterId = event.ResourceProperties.CacheClusterId;
+
+    var AWS = require('aws-sdk');
+    var elasticache = new AWS.ElastiCache({'region': 'us-east-1'});
+
+    var res = {};
+
+    var params = {
+        CacheClusterId: CacheClusterId,
+        ShowCacheNodeInfo: true
+    };
+    elasticache.describeCacheClusters(params, function (err, data) {
+        if (err) {
+            res.Error = 'describeCacheClusters call failed';
+            console.log(res.Error + ':\\n', err);
+            response.send(event, context, response.FAILED, res);
+        } else {
+            if (data.CacheClusters.length == 0) {
+                res.Error = 'Cache cluster not found';
+                console.log(res.Error + ':\\n', err);
+                response.send(event, context, response.FAILED, res);
+            }
+            if (data.CacheClusters[0].CacheNodes.length == 0) {
+                res.Error = 'Cache node not found';
+                console.log(res.Error + ':\\n', err);
+                response.send(event, context, response.FAILED, res);
+            }
+            res.EndpointAddress = data.CacheClusters[0].CacheNodes[0].Endpoint.Address;
+            console.log(res);
+            response.send(event, context, response.SUCCESS, res);
+        }
+    });
+};
