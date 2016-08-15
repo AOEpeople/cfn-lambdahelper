39fd83ed52c0a7fb76394b5a1c239f7a043cc26d
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-08-15 17:32:32 +0000

    Revert "Switched to inline function instead of cfn-response (like in cfn example)"
    This reverts commit ff09a9498d3a5af80555c66486b3aec1ba438ca5.

--- password-generator/function.js
+++ password-generator/function.js
@@ -1,45 +1,4 @@
-// Sends a response to the pre-signed S3 URL
-var sendResponse = function(event, context, responseStatus, responseData) {
-    var responseBody = JSON.stringify({
-        Status: responseStatus,
-        Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
-        PhysicalResourceId: context.logStreamName,
-        StackId: event.StackId,
-        RequestId: event.RequestId,
-        LogicalResourceId: event.LogicalResourceId,
-        Data: responseData
-    });
-
-    console.log('RESPONSE BODY:\n', responseBody);
-
-    var parsedUrl = url.parse(event.ResponseURL);
-    var options = {
-        hostname: parsedUrl.hostname,
-        port: 443,
-        path: parsedUrl.path,
-        method: 'PUT',
-        headers: {
-            'Content-Type': '',
-            'Content-Length': responseBody.length
-        }
-    };
-
-    var req = https.request(options, function(res) {
-        console.log('STATUS:', res.statusCode);
-        console.log('HEADERS:', JSON.stringify(res.headers));
-        context.succeed('Successfully sent stack response!');
-    });
-
-    req.on('error', function(err) {
-        console.log('sendResponse Error:\n', err);
-        context.fail(err);
-    });
-
-    req.write(responseBody);
-    req.end();
-};
-
-
+var response = require('cfn-response');
 exports.handler = function (event, context) {
 
     function randomPassword(length) {
@@ -57,10 +16,10 @@ exports.handler = function (event, context) {
 
     console.log('REQUEST RECEIVED:\\n', JSON.stringify(event));
     if (event.RequestType == 'Update') {
-        sendResponse(event, context, 'FAILED', {"message": "Update is not supported"});
+        r.send(event, context, r.FAILED, {"message": "Update is not supported"});
     }
     if (event.RequestType == 'Create') {
         res.Password = randomPassword(20);
     }
-    sendResponse(event, context, 'SUCCESS', res);
+    response.send(event, context, response.SUCCESS, res);
 };
